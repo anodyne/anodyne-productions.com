48f01ccb1586918b388dc0dffdd2d680408d3e9b
David VanScott <david.vanscott@gmail.com>
2020-03-18 13:02:36 -0400

    Delete all the stuff

--- resources/views/components/page-header.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="flex items-center justify-between mb-8">
-    <h1 class="font-extrabold text-blue-dark">{{ $slot }}</h1>
-
-    @if (isset($extra))
-        {!! $extra !!}
-    @endif
-</div>
--- routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::file(base_path('domain/Account/routes/web.php'));
-Route::file(base_path('domain/Auth/routes/web.php'));
-Route::file(base_path('domain/Marketing/routes/web.php'));
-Route::file(base_path('domain/Support/routes/web.php'));
