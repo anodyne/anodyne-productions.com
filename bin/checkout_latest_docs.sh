6001081a12117e8013420786bbcc4cd816c4fb29
David VanScott <david.vanscott@gmail.com>
2020-12-26 22:40:36 -0500

    Working on better deployment script

--- bin/checkout_latest_docs.sh
+++ bin/checkout_latest_docs.sh
@@ -13,4 +13,7 @@ for v in "${DOCS_VERSIONS[@]}"; do
         echo "Cloning $v..."
         git clone --single-branch --branch "$v" git@github.com:anodyne/docs.git "resources/docs/$v"
     fi;
+
+    echo "Moving images for $v..."
+    (rm -r public/docs/$v && cp -r resources/docs/$v/images public/docs/$v)
 done
