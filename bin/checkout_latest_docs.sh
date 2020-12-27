332f862e5c15973fa769a68e394546efb787becc
David VanScott <david.vanscott@gmail.com>
2020-12-26 23:15:23 -0500

    More updates

---
+++ bin/checkout_latest_docs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DOCS_VERSIONS=(
+  3.0
+  2.6
+)
+
+for v in "${DOCS_VERSIONS[@]}"; do
+    if [ -d "resources/docs/$v" ]; then
+        echo "Pulling latest documentation updates for $v..."
+        (cd resources/docs/$v && git pull)
+    else
+        echo "Cloning $v..."
+        git clone --single-branch --branch "$v" git@github.com:anodyne/docs.git "resources/docs/$v"
+    fi;
+
+    if [ -d "public/docs/$v" ]; then
+        echo "Removing images for $v..."
+        (rm -r public/docs/$v)
+    fi;
+
+    if [ -d "public/docs/$v/images" ]; then
+        echo "Moving images for $v..."
+        (cp -r resources/docs/$v/images public/docs/$v)
+    fi;
+done
