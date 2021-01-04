ea0b8ebe467f6185a3c3b3b169412497c54ccb5b
David VanScott <david.vanscott@gmail.com>
2021-01-04 16:57:05 -0500

    Fixes

--- domain/Docs/CommonMark/Extension/Alert/AlertRenderer.php
+++ domain/Docs/CommonMark/Extension/Alert/AlertRenderer.php
@@ -2,13 +2,11 @@
 
 namespace Domain\Docs\CommonMark\Extension\Alert;
 
-use BladeUI\Icons\Svg;
 use League\CommonMark\Block\Element\AbstractBlock;
 use League\CommonMark\Block\Element\FencedCode;
 use League\CommonMark\Block\Renderer\BlockRendererInterface;
 use League\CommonMark\ElementRendererInterface;
 use League\CommonMark\HtmlElement;
-use League\CommonMark\Util\Xml;
 
 class AlertRenderer implements BlockRendererInterface
 {
@@ -30,47 +28,44 @@ class AlertRenderer implements BlockRendererInterface
         $infoWords = $block->getInfoWords();
         $type = $infoWords[0];
 
-        if (\count($infoWords) !== 0 && \strlen($type) !== 0) {
+        unset($infoWords[0]);
+        $title = count($infoWords) > 0 ? implode(' ', $infoWords) : null;
+
+        if (\strlen($type) !== 0) {
             $attrs['class'] = isset($attrs['class']) ? $attrs['class'].' ' : '';
             $attrs['class'] .= "is-{$type}";
         }
 
-        return new HtmlElement(
-            'div',
-            $attrs,
-            [
-                new HtmlElement('div', ['class' => 'accent'], null),
-                new HtmlElement('div', ['class' => 'identifier'], [
-                    svg($this->getIconForType($type))->class('h-6 w-6')->toHtml(),
-                    new HtmlElement('span', [], ucfirst($type)),
-                ]),
-                new HtmlElement('div', ['class' => 'content'], Xml::escape($block->getStringContent())),
-            ]
-        );
+        $filling = [];
+        $filling[] = new HtmlElement('div', ['class' => 'accent'], null);
+
+        if ($type) {
+            $filling[] = new HtmlElement('div', ['class' => 'identifier'], [
+                svg($this->getIconForType($type))->class('h-6 w-6')->toHtml(),
+                new HtmlElement('span', [], $title ?? ucfirst($type)),
+            ]);
+        }
+
+        $filling[] = new HtmlElement('div', ['class' => 'content'], $htmlRenderer->renderInlines($block->children()));
+
+        return new HtmlElement('div', $attrs, $filling);
     }
 
     protected function getIconForType(string $type): string
     {
         switch ($type) {
             case 'note':
-            case 'info':
             default:
                 return 'fluent-info';
 
                 break;
 
             case 'warning':
-            case 'danger':
-            case 'failure':
-            case 'caution':
-            case 'attention':
-            case 'error':
                 return 'fluent-warning';
 
                 break;
 
             case 'tip':
-            case 'hint':
                 return 'fluent-flash';
 
                 break;
@@ -86,8 +81,6 @@ class AlertRenderer implements BlockRendererInterface
                 break;
 
             case 'success':
-            case 'done':
-            case 'check':
                 return 'fluent-checkmark-circle';
 
                 break;
