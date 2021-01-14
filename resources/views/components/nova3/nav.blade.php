2006696f03b76ff93cdfd48cf4a17b756cdb27b0
David VanScott <david.vanscott@gmail.com>
2021-01-14 11:03:18 -0500

    Logo updates to get rid of Tailwind logo

--- resources/views/components/nova3/nav.blade.php
+++ resources/views/components/nova3/nav.blade.php
@@ -3,7 +3,7 @@
         <div class="flex items-center justify-between w-full md:w-auto">
             <a href="/">
                 <span class="sr-only">Anodyne</span>
-                <x-logos.anodyne-mark class="h-8 w-auto sm:h-10 text-anodyne-orange-4" />
+                <x-logos.anodyne class="h-8 w-auto sm:h-10" />
             </a>
             <div class="-mr-2 flex items-center md:hidden">
                 <button type="button" class="bg-gray-800 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-700 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white" id="main-menu" aria-haspopup="true" @click="open = true">
@@ -24,7 +24,7 @@
 
             {{-- <a href="#download" class="font-medium text-white hover:text-gray-300">Download</a> --}}
 
-            {{-- <a href="{{ route('docs') }}" class="font-medium text-white hover:text-gray-300">Docs</a> --}}
+            {{-- <a href="{{ route('docs') }}" class="font-medium text-white hover:text-gray-300">Documentation</a> --}}
 
             <a href="https://discord.gg/7WmKUks" target="_blank" class="font-medium text-white hover:text-gray-300">Join Discord</a>
         </div>
@@ -43,7 +43,7 @@
     <div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
         <div class="px-5 pt-4 flex items-center justify-between">
             <div>
-                <x-logos.anodyne-mark class="h-8 w-auto text-anodyne-orange-4" />
+                <x-logos.anodyne text-color="gray-600" class="h-8 w-auto" />
             </div>
             <div class="-mr-2">
                 <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-anodyne-orange-4" @click="open = false">
@@ -65,7 +65,7 @@
 
                 {{-- <a href="#download" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">Download</a> --}}
 
-                {{-- <a href="{{ route('docs') }}" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">Docs</a> --}}
+                {{-- <a href="{{ route('docs') }}" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">Documentation</a> --}}
 
                 <a href="https://discord.gg/7WmKUks" target="_blank" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">Join Discord</a>
             </div>
