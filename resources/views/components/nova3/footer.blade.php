256a80baa47658bbc5c4095e684357f0f62fca2d
David VanScott <david.vanscott@gmail.com>
2021-01-16 09:31:46 -0500

    Footer updates

--- resources/views/components/nova3/footer.blade.php
+++ resources/views/components/nova3/footer.blade.php
@@ -3,34 +3,39 @@
         <div class="flex items-center justify-center space-x-6 md:order-2">
             <a href="https://facebook.com/anodyneproductions" target="_blank" class="text-gray-500 hover:text-gray-400 transition-all ease-in-out duration-200">
                 <span class="sr-only">Facebook</span>
-                <x-logos.facebook class="h-6 w-6" />
+                <x-logos.facebook class="h-8 w-8 md:h-6 md:w-6" />
             </a>
 
             <a href="https://twitter.com/anodyneprod" target="_blank" class="text-gray-500 hover:text-gray-400 transition-all ease-in-out duration-200">
                 <span class="sr-only">Twitter</span>
-                <x-logos.twitter class="h-6 w-6" />
+                <x-logos.twitter class="h-8 w-8 md:h-6 md:w-6" />
             </a>
 
             <a href="https://github.com/anodyne" target="_blank" class="text-gray-500 hover:text-gray-400 transition-all ease-in-out duration-200">
                 <span class="sr-only">GitHub</span>
-                <x-logos.github class="h-6 w-6" />
+                <x-logos.github class="h-8 w-8 md:h-6 md:w-6" />
             </a>
 
             <a href="https://discord.gg/7WmKUks" target="_blank" class="text-gray-500 hover:text-gray-400 transition-all ease-in-out duration-200">
                 <span class="sr-only">Discord</span>
-                <x-logos.discord class="h-6 w-auto" />
+                <x-logos.discord class="h-8 md:h-6 w-auto" />
             </a>
 
             <a href="https://patreon.com/anodyneproductions" target="_blank" class="text-gray-500 hover:text-gray-400 transition-all ease-in-out duration-200">
                 <span class="sr-only">Patreon</span>
-                <x-logos.patreon class="h-4 w-auto" />
+                <x-logos.patreon class="h-6 md:h-4 w-auto" />
             </a>
         </div>
-        <div class="mt-8 md:mt-0 md:order-1">
-            <p class="flex items-center justify-center md:justify-start space-x-4 text-center text-base text-gray-500">
-                <x-logos.anodyne-mark class="h-6 w-auto" color="gray-600" />
-                <span>&copy; {{ date('Y') }} Anodyne Productions. All rights reserved.</span>
-            </p>
+        <div class="md:order-1">
+            <div class="flex flex-col md:flex-row items-center justify-center md:justify-start space-y-8 md:space-y-0 md:space-x-4">
+                <x-logos.anodyne-mark color="gray-600" class="hidden md:block h-6 w-auto" />
+
+                <p class="text-center text-base text-gray-500">
+                    &copy; {{ date('Y') }} Anodyne Productions. All rights reserved.
+                </p>
+
+                <x-logos.anodyne logo-color="gray-600" text-color="gray-600" class="block md:hidden h-6 w-auto" />
+            </div>
         </div>
     </div>
 </footer>
