804b0acca64804bb18ce05c2f23ab614ce02a970
David VanScott <david.vanscott@gmail.com>
2021-01-16 10:19:52 -0500

    More updates

--- resources/views/components/nova2/nav.blade.php
+++ resources/views/components/nova2/nav.blade.php
@@ -9,7 +9,7 @@
                 <button type="button" class="bg-gray-800 rounded-lg p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-700 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white" id="main-menu" aria-haspopup="true" @click="open = true">
                     <span class="sr-only">Open main menu</span>
                     <!-- Heroicon name: menu -->
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <svg class="h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
                 </button>
