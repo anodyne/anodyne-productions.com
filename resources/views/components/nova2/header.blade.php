f476f969c0413a37703a6bb7f7f2122d4957791f
David VanScott <david.vanscott@gmail.com>
2021-03-01 08:53:37 -0500

    Header updates

--- resources/views/components/nova2/header.blade.php
+++ resources/views/components/nova2/header.blade.php
@@ -8,8 +8,8 @@
                     <div class="px-4 sm:px-6 lg:px-8 sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left lg:flex lg:items-center">
                         <div>
                             <a href="{{ route('nova-3') }}" class="inline-flex items-center text-white bg-gray-900 rounded-full p-1 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-gray-200">
-                                <span class="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-gradient-to-r from-orange-500 to-amber-500 rounded-full">Nova 3 is coming</span>
-                                <span class="ml-4 text-sm">Visit the preview page</span>
+                                <span class="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-gradient-to-r from-orange-500 to-amber-500 rounded-full">The future of Nova</span>
+                                <span class="ml-4 text-sm">Visit the Nova 3 preview page</span>
 
                                 <svg class="ml-2 w-5 h-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                     <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
@@ -17,10 +17,10 @@
                             </a>
                             <h1 class="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:leading-none lg:mt-6 lg:text-5xl xl:text-6xl">
                                 <span class="block">Painless</span>
-                                <span class="block text-amber-500">RPG management</span>
+                                <span class="block bg-clip-text text-transparent leading-tight bg-gradient-to-r from-orange-500 to-amber-500">RPG management</span>
                             </h1>
                             <p class="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-                                With an easy-to-use interface, powerful posting system, a wide array of developer tools and much more, Nova is all you need to help you stop managing your game and get back to playing it.
+                                With an easy-to-use interface, integrated posting system, a wide array of developer tools and much more, Nova is all you need to stop managing your game and get back to playing it.
                             </p>
                         </div>
                     </div>
