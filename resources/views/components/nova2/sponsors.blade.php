1ecab2857c468fc24ef31a608b9320c0075a7b84
David VanScott <david.vanscott@gmail.com>
2021-03-01 08:53:52 -0500

    Sponsor updates

--- resources/views/components/nova2/sponsors.blade.php
+++ resources/views/components/nova2/sponsors.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-        <div class="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
+        <div class="lg:grid lg:grid-cols-2 lg:gap-8">
             <div>
                 <h2 class="text-3xl font-extrabold text-gray-900 sm:text-4xl">
                     Thanks to our incredible sponsors
@@ -16,34 +16,35 @@
                     </div>
                 </div>
             </div>
-            <div class="mt-8 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-0 lg:grid-cols-2">
+            <div class="mt-8 grid grid-cols-2 gap-x-0.5 gap-y-12 md:grid-cols-3 lg:mt-0 lg:grid-cols-2">
                 <div class="col-span-1 flex justify-center px-8">
                     <a href="https://blackhawk.anurasims.com/" target="_blank" class="flex flex-col space-y-4 font-extrabold text-xl text-center hover:text-amber-500 transition-colors ease-in-out duration-150">
-                        <img class="max-h-48" src="{{ Storage::url('images/sponsors/blackhawklogo.png') }}" alt="USS Blackhawk logo">
-                        <span>USS Blackhawk</span>
+                        <img class="h-48 w-auto" src="{{ Storage::url('images/sponsors/logo_sim_central.svg') }}" alt="Sim Central logo">
+                        <div class="flex flex-col space-y-1">
+                            <span>Sim Central</span>
+                            <span class="text-sm font-medium text-gray-400">Platinum sponsor</span>
+                        </div>
                     </a>
                 </div>
                 <div class="col-span-1 flex justify-center px-8">
                     <a href="https://blackhawk.anurasims.com/" target="_blank" class="flex flex-col space-y-4 font-extrabold text-xl text-center hover:text-amber-500 transition-colors ease-in-out duration-150">
-                        <img class="max-h-48" src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg" alt="Sim Central logo">
-                        <span>Sim Central</span>
+                        <img class="h-48 w-auto" src="{{ Storage::url('images/sponsors/blackhawklogo.png') }}" alt="USS Blackhawk logo">
+                        <div class="flex flex-col space-y-1">
+                            <span>USS Black Hawk</span>
+                            <span class="text-sm font-medium text-gray-400">Gold sponsor</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-span-1 flex justify-center px-8">
+                    <a href="https://portal.5thfleet.net/" target="_blank" class="flex flex-col space-y-4 font-extrabold text-xl text-center hover:text-amber-500 transition-colors ease-in-out duration-150">
+                        <img class="h-48 w-auto" src="{{ Storage::url('images/sponsors/blackhawklogo.png') }}" alt="USS Blackhawk logo">
+                        <div class="flex flex-col space-y-1">
+                            <span>Fifth Fleet</span>
+                            <span class="text-sm font-medium text-gray-400">Gold sponsor</span>
+                        </div>
                     </a>
                 </div>
             </div>
         </div>
     </div>
 </div>
-
-{{-- <div>
-    <div class="max-w-3xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
-        <h2 class="text-3xl font-extrabold text-gray-900 sm:text-4xl">
-            <span class="block">Thanks to our incredible sponsors</span>
-        </h2>
-        <p class="mt-4 text-lg text-gray-500">
-            We've launched a Patreon as a way for people to support Anodyne and help us continue to provide Nova and all of its resources to the community for free. As a patron, you'll have access to a private Discord community, early access to Nova 3, regular updates on Anodyne's products and services, and more. Join today!
-        </p>
-        <a href="https://patreon.com/anodyneproductions" target="_blank" class="mt-8 bg-white border border-transparent rounded-lg shadow px-6 py-3 inline-flex items-center justify-center space-x-3 text-base font-medium text-orange-500 hover:bg-orange-50 w-full lg:w-auto transition-colors ease-in-out duration-200">
-            Become a patron
-        </a>
-    </div>
-</div> --}}
