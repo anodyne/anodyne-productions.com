e94995ce750f2cc95616a3100f3911beb1c6838d
David VanScott <david.vanscott@gmail.com>
2021-04-15 11:01:10 -0400

    Fix some spacing issues

--- resources/views/components/nova3/header.blade.php
+++ resources/views/components/nova3/header.blade.php
@@ -7,9 +7,9 @@
                 <div class="lg:grid lg:grid-cols-12 lg:gap-8">
                     <div class="px-4 sm:px-6 lg:px-8 sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left lg:flex lg:items-center">
                         <div>
-                            <h1 class="text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:leading-none lg:mt-6 lg:text-5xl xl:text-6xl">
-                                <span class="block">Nova 3:</span>
-                                <span class="block bg-clip-text text-transparent bg-gradient-to-r from-orange-500 to-amber-500">
+                            <h1 class="text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:leading-none lg:mt-6 lg:text-5xl">
+                                <span class="block xl:text-6xl">Nova 3:</span>
+                                <span class="mt-1 block bg-clip-text text-transparent bg-gradient-to-r from-orange-500 to-amber-500">
                                     The Next Generation
                                 </span>
                             </h1>
