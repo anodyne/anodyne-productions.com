ba91c11937c069b29bbc4313051b179ab8df9bde
David VanScott <david.vanscott@gmail.com>
2022-03-23 15:26:15 -0400

    Updates

--- resources/views/nova-3.blade.php
+++ resources/views/nova-3.blade.php
@@ -26,21 +26,21 @@
                     <div class="flex space-x-8">
                         <a role="button" @click.prevent="feature = 'chronological'" class="flex-1 flex flex-col space-y-1 pb-6 border-b-[3px] transition" :class="{ 'border-white': feature === 'chronological', 'border-transparent hover:border-gray-500': feature !== 'chronological' }">
                             <span :class="{ 'text-gray-500': feature !== 'chronological', 'text-amber-500': feature === 'chronological' }">
-                                @svg('fluent-timeline', 'h-6 w-6')
+                                @svg('fluent-timeline', 'h-7 w-7')
                             </span>
                             <div class="font-semibold text-white">Chronological context</div>
                             <p class="text-gray-500">Stories live on a timeline, giving readers the chance to experience your game's adventures in exactly the order you intend them.</p>
                         </a>
                         <a role="button" @click.prevent="feature = 'non-linear'" class="flex-1 flex flex-col space-y-1 pb-6 border-b-[3px] transition" :class="{ 'border-white': feature === 'non-linear', 'border-transparent hover:border-gray-500': feature !== 'non-linear' }">
                             <span :class="{ 'text-gray-500': feature !== 'non-linear', 'text-amber-500': feature === 'non-linear' }">
-                                @svg('fluent-clock', 'h-6 w-6')
+                                @svg('fluent-clock', 'h-7 w-7')
                             </span>
                             <div class="font-semibold text-white">Non-linear storytelling</div>
                             <p class="text-gray-500">Add stories and posts in whatever order you want to ensure chronological integrity. You can even add stories <em>inside</em> of other stories.</p>
                         </a>
                         <a role="button" @click.prevent="feature = 'post-types'" class="flex-1 flex flex-col space-y-1 pb-6 border-b-[3px] transition" :class="{ 'border-white': feature === 'post-types', 'border-transparent hover:border-gray-500': feature !== 'post-types' }">
                             <span :class="{ 'text-gray-500': feature !== 'post-types', 'text-amber-500': feature === 'post-types' }">
-                                @svg('fluent-edit-settings', 'h-6 w-6')
+                                @svg('fluent-edit-settings', 'h-7 w-7')
                             </span>
                             <div class="font-semibold text-white">Post types</div>
                             <p class="text-gray-500">Create different ways for players to contribute through post types with fine-tuned control over a host of options for any post of that type.</p>
@@ -76,21 +76,21 @@
                     <div class="flex space-x-8">
                         <a role="button" @click.prevent="feature = 'any-device'" class="flex-1 flex flex-col space-y-1 pb-6 border-b-[3px] transition" :class="{ 'border-white': feature === 'any-device', 'border-transparent hover:border-gray-500': feature !== 'any-device' }">
                             <span :class="{ 'text-gray-500': feature !== 'any-device', 'text-amber-500': feature === 'any-device' }">
-                                @svg('fluent-phone-desktop', 'h-6 w-6')
+                                @svg('fluent-phone-desktop', 'h-7 w-7')
                             </span>
                             <div class="font-semibold text-white">Bring any device</div>
                             <p class="text-gray-500">Whether you're playing on a mobile phone or an 4k monitor, Nova is designed to let you manage and play your game from any device.</p>
                         </a>
                         <a role="button" @click.prevent="feature = 'mobile'" class="flex-1 flex flex-col space-y-1 pb-6 border-b-[3px] transition" :class="{ 'border-white': feature === 'mobile', 'border-transparent hover:border-gray-500': feature !== 'mobile' }">
                             <span :class="{ 'text-gray-500': feature !== 'mobile', 'text-amber-500': feature === 'mobile' }">
-                                @svg('fluent-paint-bucket', 'h-6 w-6')
+                                @svg('fluent-paint-bucket', 'h-7 w-7')
                             </span>
                             <div class="font-semibold text-white">Mobile-specific designs</div>
                             <p class="text-gray-500">We took cues from mobile operating systems so that Nova feels natural when you're using it on your phone or tablet.</p>
                         </a>
                         <a role="button" @click.prevent="feature = 'pwa'" class="flex-1 flex flex-col space-y-1 pb-6 border-b-[3px] transition" :class="{ 'border-white': feature === 'pwa', 'border-transparent hover:border-gray-500': feature !== 'pwa' }">
                             <span :class="{ 'text-gray-500': feature !== 'pwa', 'text-amber-500': feature === 'pwa' }">
-                                @svg('fluent-phone-mobile', 'h-6 w-6')
+                                @svg('fluent-phone-mobile', 'h-7 w-7')
                             </span>
                             <div class="font-semibold text-white">Progressive web app</div>
                             <p class="text-gray-500">Nova 3 sites can be added to your home screen and will act and behave more like a native app than a webpage.</p>
@@ -126,21 +126,21 @@
                     <div class="flex space-x-8">
                         <a role="button" @click.prevent="feature = 'groups'" class="flex-1 flex flex-col space-y-1 pb-6 border-b-[3px] transition" :class="{ 'border-white': feature === 'groups', 'border-transparent hover:border-gray-500': feature !== 'groups' }">
                             <span :class="{ 'text-gray-500': feature !== 'groups', 'text-amber-500': feature === 'groups' }">
-                                @svg('fluent-group-list', 'h-6 w-6')
+                                @svg('fluent-group-list', 'h-7 w-7')
                             </span>
                             <div class="font-semibold text-white">Rank groups</div>
                             <p class="text-gray-500">Easily create groups to organize your ranks and make it easy to find the ranks you're looking for when you need them.</p>
                         </a>
                         <a role="button" @click.prevent="feature = 'info'" class="flex-1 flex flex-col space-y-1 pb-6 border-b-[3px] transition" :class="{ 'border-white': feature === 'info', 'border-transparent hover:border-gray-500': feature !== 'info' }">
                             <span :class="{ 'text-gray-500': feature !== 'info', 'text-amber-500': feature === 'info' }">
-                                @svg('fluent-arrow-repeat-all', 'h-6 w-6')
+                                @svg('fluent-arrow-repeat-all', 'h-7 w-7')
                             </span>
                             <div class="font-semibold text-white">Re-usable rank information</div>
                             <p class="text-gray-500">Save time by re-using basic rank information like name across multiple ranks. Change it once and it's updated everywhere.</p>
                         </a>
                         <a role="button" @click.prevent="feature = 'composition'" class="flex-1 flex flex-col space-y-1 pb-6 border-b-[3px] transition" :class="{ 'border-white': feature === 'composition', 'border-transparent hover:border-gray-500': feature !== 'composition' }">
                             <span :class="{ 'text-gray-500': feature !== 'composition', 'text-amber-500': feature === 'composition' }">
-                                @svg('fluent-layer', 'h-6 w-6')
+                                @svg('fluent-layer', 'h-7 w-7')
                             </span>
                             <div class="font-semibold text-white">Rank composition</div>
                             <p class="text-gray-500">Nova 3 sites can be added to your home screen and will act and behave more like a native app than a webpage.</p>
