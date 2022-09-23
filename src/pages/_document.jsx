95b5c0606c7416ce46500316f12989c2f1f4a35c
David VanScott <david.vanscott@gmail.com>
2022-09-23 16:28:56 -0400

    Updates

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -60,10 +60,6 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Lexend:wght@400;500&display=swap" />
-
                 <script dangerouslySetInnerHTML={{ __html: themeScript }} />
             </Head>
             <body>
