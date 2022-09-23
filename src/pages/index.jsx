f99935f8ddd9e221cc3f58c708191d90db2a8ed8
David VanScott <david.vanscott@gmail.com>
2022-09-23 16:14:33 -0400

    Reversing

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,7 +12,7 @@ export default function Home() {
         <>
             <Header />
             <Hero />
-            {/* <Features /> */}
+            <Features />
             <Download />
             <Resources />
             <Sponsors />
