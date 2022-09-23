999f394c710f46a394f10f7e0fadde2bc19f78f8
David VanScott <david.vanscott@gmail.com>
2022-09-23 15:49:04 -0400

    More fixes

--- bin/checkout_latest_docs.sh
+++ bin/checkout_latest_docs.sh
@@ -9,7 +9,7 @@ DOCS_VERSIONS=(
 for v in "${DOCS_VERSIONS[@]}"; do
     if [ -d "src/pages/docs/$v" ]; then
         echo "Pulling latest documentation updates for $v..."
-        (cd src/pages/docs/$v && git reset HEAD --hard && git pull)
+        (cd src/pages/docs/"$v" && git reset HEAD --hard && git pull)
     else
         echo "Cloning $v..."
         git clone --single-branch --branch "$v" git@github.com:anodyne/docs.git "src/pages/docs/$v"
@@ -17,12 +17,12 @@ for v in "${DOCS_VERSIONS[@]}"; do
 
     if [ -d "public/images/docs/$v" ]; then
         echo "Removing images for $v..."
-        (rm -r public/images/docs/$v)
+        (rm -r public/images/docs/"$v")
     fi;
 
     if [ -d "src/pages/docs/$v/images" ]; then
         echo "Moving images for $v..."
-        (mkdir public/images/docs/$v)
-        (cp -a src/pages/docs/$v/images public/images/docs/$v)
+        (mkdir public/images/docs/"$v")
+        (cp -a src/pages/docs/"$v"/images public/images/docs/"$v")
     fi;
 done
