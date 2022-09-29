43252907c0bbafd32c6cebe5bd37f55ce0dba9c4
David VanScott <david.vanscott@gmail.com>
2022-09-29 13:23:02 -0400

    More updates

--- src/components/marketing/nova2/Features.jsx
+++ src/components/marketing/nova2/Features.jsx
@@ -1,94 +1,69 @@
-import { useId } from 'react'
-import Image from 'next/future/image'
-import { Tab } from '@headlessui/react'
 import clsx from 'clsx'
 import { Container } from '@/components/marketing/Container'
-import screenshotContacts from '@/images/screenshots/contacts.png'
-import screenshotInventory from '@/images/screenshots/inventory.png'
-import screenshotProfitLoss from '@/images/screenshots/profit-loss.png'
 
 const features = [
   {
+    name: 'All-in-one website',
+    description: 'A dedicated website with all of your content lets you easily show off your game to the world.',
+    icon: function WebsiteIcon() {
+      return (
+        <>
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M7.00004 6.84152C5.61083 5.13811 4.14406 6.34846 4.12476 7.65318C4.12476 9.59556 6.45032 11.1899 7.00004 11.1899C7.54976 11.1899 9.87534 9.59556 9.87534 7.65318C9.85604 6.34846 8.38925 5.13811 7.00004 6.84152Z" /><path stroke="currentColor" d="M0.959867 10.2685C1.114 11.7092 2.2727 12.8679 3.71266 13.0284C4.78221 13.1476 5.88037 13.25 7 13.25C8.11963 13.25 9.21779 13.1476 10.2873 13.0284C11.7273 12.8679 12.886 11.7092 13.0401 10.2685C13.1539 9.20502 13.25 8.11315 13.25 7C13.25 5.88684 13.1539 4.79498 13.0401 3.73147C12.886 2.29082 11.7273 1.13211 10.2873 0.971609C9.21779 0.852392 8.11963 0.75 7 0.75C5.88037 0.75 4.78221 0.852392 3.71266 0.971609C2.2727 1.13211 1.114 2.29082 0.959867 3.73147C0.846083 4.79498 0.75 5.88684 0.75 7C0.75 8.11315 0.846084 9.20502 0.959867 10.2685Z" /><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M13.0684 4L0.931641 4C0.9409 3.91029 0.95033 3.82078 0.959886 3.73147C1.11402 2.29082 2.27272 1.13211 3.71267 0.971609C4.78222 0.852392 5.88039 0.75 7.00002 0.75C8.11965 0.75 9.21781 0.852392 10.2874 0.971609C11.7273 1.13211 12.886 2.29082 13.0402 3.73147C13.0497 3.82078 13.0591 3.91029 13.0684 4Z" />
+        </>
+      )
+    },
+  },
+  {
+    name: 'Easy character management',
+    description: "Manage all of your game's characters in one place and let players take ownership of the characters they play.",
+    icon: function CharactersIcon() {
+      return (
+        <>
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M4.82872 10.0856C4.51879 10.0806 4.21446 10.0023 3.94069 9.85691C2.99289 9.36602 2.20032 8.62117 1.65159 7.70564C1.10286 6.79011 0.8196 5.74 0.833488 4.67271L0.863159 2.07249C2.11535 1.41748 3.51057 1.08361 4.92362 1.10083C6.33667 1.11805 7.72334 1.48583 8.95919 2.17116L8.93484 4.16376" /><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M8.24858 13.0206C7.94624 13.089 7.63201 13.0846 7.33165 13.0081C7.0313 12.9315 6.75337 12.7848 6.52067 12.58C5.71222 11.8831 5.11357 10.975 4.79164 9.95734C4.4697 8.93967 4.43719 7.85251 4.69772 6.81742L5.32841 4.29466C6.6982 3.94726 8.13281 3.94538 9.50351 4.28918C10.8742 4.63299 12.1381 5.31173 13.1818 6.26448L12.5511 8.7786C12.2968 9.81463 11.76 10.7597 11.0004 11.5087C10.2407 12.2577 9.28811 12.7811 8.24858 13.0206Z" /><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M9.64184 8.17147C9.96075 8.02893 10.3991 8.09821 10.6309 8.21423" /><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M7.61284 7.63825C7.40669 7.33219 6.99638 7.22809 6.74137 7.26779" />
+        </>
+      )
+    },
+  },
+  {
+    name: 'Tell your stories',
+    description: "An integrated story and posting system gives you and your players the freedom to tell your game's stories.",
+    icon: function StoriesIcon() {
+      return (
+        <>
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M5.0293 13.0294C4.43511 12.9989 3.86632 12.957 3.3475 12.9028C3.09788 12.9028 2.85849 12.8055 2.68198 12.6324C1.77509 11.3163 1.77026 2.50298 2.68198 1.17313C2.85849 1.00002 3.09788 0.902771 3.3475 0.902771C5.39071 0.68917 8.20894 0.667028 10.3016 0.779808C10.8208 0.807789 11.2187 1.24175 11.2187 1.76169L11.2186 5.02167" /><path stroke="currentColor" strokeLinecap="round" d="M0.75 3.90271H2.75" /><path stroke="currentColor" strokeLinecap="round" d="M0.75 9.90271H2.75" /><path stroke="currentColor" strokeLinejoin="round" d="M9.18396 13.2294L7.45245 13.3647L7.59297 11.6384L11.3574 7.72464C11.8335 7.22957 12.6234 7.22189 13.1091 7.7076V7.7076C13.5943 8.1928 13.5872 8.98163 13.0934 9.45804L9.18396 13.2294Z" />
+        </>
+      )
+    },
+  },
+  {
+    name: 'Post locking',
+    description: 'Post locking intelligently locks and unlocks multi-author posts to help prevent your changes being overwritten.',
+    icon: function PostLockingIcon() {
+      return (
+        <>
+          <path stroke="currentColor" d="M1.88768 10.6061C2.0503 12.0408 3.26353 13.1594 4.70649 13.2106C5.43845 13.2366 6.19004 13.25 7.00008 13.25C7.81013 13.25 8.56171 13.2366 9.29367 13.2106C10.7366 13.1594 11.9499 12.0408 12.1125 10.6061C12.1692 10.1061 12.2084 9.59799 12.2084 9.08335C12.2084 8.56872 12.1692 8.06065 12.1125 7.56059C11.9499 6.1259 10.7366 5.00729 9.29367 4.95608C8.56171 4.93011 7.81013 4.91669 7.00008 4.91669C6.19004 4.91669 5.43845 4.93011 4.70649 4.95608C3.26353 5.00729 2.0503 6.1259 1.88768 7.56059C1.831 8.06065 1.79175 8.56872 1.79175 9.08335C1.79175 9.59799 1.831 10.1061 1.88768 10.6061Z" /><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M9.91659 4.91667V3.66667C9.91659 2.89312 9.60929 2.15125 9.06231 1.60427C8.51533 1.05729 7.77347 0.75 6.99992 0.75C6.22637 0.75 5.48451 1.05729 4.93752 1.60427C4.39054 2.15125 4.08325 2.89312 4.08325 3.66667V4.91667" /><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M7 8.50006L7 9.5" />
+        </>
+      )
+    },
+  },
+  {
     name: 'Reporting',
-    summary: 'Stay on top of things with always up-to-date reporting features.',
-    description:
-      'We talked about reporting in the section above but we needed three items here, so mentioning it one more time for posterity.',
-    image: screenshotProfitLoss,
+    description: "Get valuable insights into activity, posting levels, and even forecasting game activity for the rest of the month.",
     icon: function ReportingIcon() {
-      let id = useId()
       return (
         <>
-          <defs>
-            <linearGradient
-              id={id}
-              x1="11.5"
-              y1={18}
-              x2={36}
-              y2="15.5"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop offset=".194" stopColor="#fff" />
-              <stop offset={1} stopColor="#6692F1" />
-            </linearGradient>
-          </defs>
-          <path
-            d="m30 15-4 5-4-11-4 18-4-11-4 7-4-5"
-            stroke={`url(#${id})`}
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M0.75 0.75V11.1205C0.75 12.1752 1.56805 13.0481 2.62142 13.1016C3.92678 13.1679 5.66667 13.2408 7 13.2408C9.44273 13.2408 13.25 12.9959 13.25 12.9959" /><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M3.5 7.86584C3.5 7.86584 5.44135 9.56008 6 9.24084C7.0853 8.62067 8.25 1.74084 9.375 1.74084C10.3125 1.74084 13 4.86584 13 4.86584" />
         </>
       )
     },
   },
   {
-    name: 'Inventory',
-    summary:
-      'Never lose track of what’s in stock with accurate inventory tracking.',
-    description:
-      'We don’t offer this as part of our software but that statement is inarguably true. Accurate inventory tracking would help you for sure.',
-    image: screenshotInventory,
-    icon: function InventoryIcon() {
+    name: 'Customize your way',
+    description: "Change the way your site looks or works with tools to customize things any way you want.",
+    icon: function CustomizableIcon() {
       return (
         <>
-          <path
-            opacity=".5"
-            d="M8 17a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1v-2Z"
-            fill="#fff"
-          />
-          <path
-            opacity=".3"
-            d="M8 24a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1v-2Z"
-            fill="#fff"
-          />
-          <path
-            d="M8 10a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1v-2Z"
-            fill="#fff"
-          />
-        </>
-      )
-    },
-  },
-  {
-    name: 'Contacts',
-    summary:
-      'Organize all of your contacts, service providers, and invoices in one place.',
-    description:
-      'This also isn’t actually a feature, it’s just some friendly advice. We definitely recommend that you do this, you’ll feel really organized and professional.',
-    image: screenshotContacts,
-    icon: function ContactsIcon() {
-      return (
-        <>
-          <path
-            opacity=".5"
-            d="M25.778 25.778c.39.39 1.027.393 1.384-.028A11.952 11.952 0 0 0 30 18c0-6.627-5.373-12-12-12S6 11.373 6 18c0 2.954 1.067 5.659 2.838 7.75.357.421.993.419 1.384.028.39-.39.386-1.02.036-1.448A9.959 9.959 0 0 1 8 18c0-5.523 4.477-10 10-10s10 4.477 10 10a9.959 9.959 0 0 1-2.258 6.33c-.35.427-.354 1.058.036 1.448Z"
-            fill="#fff"
-          />
-          <path
-            d="M12 28.395V28a6 6 0 0 1 12 0v.395A11.945 11.945 0 0 1 18 30c-2.186 0-4.235-.584-6-1.605ZM21 16.5c0-1.933-.5-3.5-3-3.5s-3 1.567-3 3.5 1.343 3.5 3 3.5 3-1.567 3-3.5Z"
-            fill="#fff"
-          />
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M9.12061 5.03643C8.30163 4.21744 8.25701 3.3407 9.93949 1.65822C10.0058 1.59192 9.99288 1.48046 9.91087 1.43501C8.39873 0.596888 6.92288 0.790403 5.67858 2.0347C4.51064 3.20265 4.26848 4.57459 4.93626 5.98907C4.96086 6.04119 4.94735 6.10361 4.90434 6.14198C4.31992 6.66338 3.72283 7.20397 3.1422 7.7846C2.56157 8.36523 2.02098 8.96232 1.49958 9.54674C0.878328 10.2431 0.744926 11.3273 1.32716 12.0566C1.41832 12.1708 1.51503 12.2812 1.61932 12.3854C1.7236 12.4897 1.83394 12.5864 1.94812 12.6776C2.67742 13.2598 3.76166 13.1264 4.45801 12.5052C5.04243 11.9838 5.63952 11.4432 6.22015 10.8626C6.80078 10.2819 7.34137 9.68483 7.86277 9.10041C7.90114 9.0574 7.96357 9.04389 8.01569 9.06849C9.43016 9.73626 10.8021 9.49409 11.97 8.32616C13.1743 7.12192 13.3943 5.70081 12.6475 4.23983C12.6188 4.18363 12.5435 4.17293 12.4988 4.21756C10.8163 5.90003 9.9396 5.85542 9.12061 5.03643Z" />
         </>
       )
     },
@@ -97,111 +72,17 @@ const features = [
 
 function Feature({ feature, isActive, className, ...props }) {
   return (
-    <div
-      className={clsx(className, !isActive && 'opacity-75 hover:opacity-100')}
-      {...props}
-    >
-      <div
-        className={clsx(
-          'w-9 rounded-lg',
-          isActive ? 'bg-sky-500' : 'bg-slate-500'
-        )}
-      >
-        <svg aria-hidden="true" className="h-9 w-9" fill="none">
+    <div {...props}>
+      <div className="inline-flex p-2.5 rounded-lg bg-slate-400">
+        <svg aria-hidden="true" className="h-6 w-6 text-white" fill="none" viewBox="0 0 14 14">
           <feature.icon />
         </svg>
       </div>
-      <h3
-        className={clsx(
-          'mt-6 text-sm font-medium',
-          isActive ? 'text-sky-600' : 'text-slate-600'
-        )}
-      >
+      <p className="mt-4 font-display text-xl text-slate-800">
         {feature.name}
-      </h3>
-      <p className="mt-2 font-display text-xl text-slate-800">
-        {feature.summary}
       </p>
-      <p className="mt-4 text-sm text-slate-600">{feature.description}</p>
+      <p className="mt-2 text-sm text-slate-600">{feature.description}</p>
     </div>
-  )
-}
-
-function FeaturesMobile() {
-  return (
-    <div className="-mx-4 mt-20 flex flex-col gap-y-10 overflow-hidden px-4 sm:-mx-6 sm:px-6 lg:hidden">
-      {features.map((feature) => (
-        <div key={feature.name}>
-          <Feature feature={feature} className="mx-auto max-w-2xl" isActive />
-          <div className="relative mt-10 pb-10">
-            <div className="absolute -inset-x-4 bottom-0 top-8 bg-slate-200 sm:-inset-x-6" />
-            <div className="relative mx-auto w-[52.75rem] overflow-hidden rounded-xl bg-white shadow-lg shadow-slate-900/5 ring-1 ring-slate-500/10">
-              <Image
-                className="w-full"
-                src={feature.image}
-                alt=""
-                sizes="52.75rem"
-              />
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  )
-}
-
-function FeaturesDesktop() {
-  return (
-    <Tab.Group as="div" className="hidden lg:mt-20 lg:block">
-      {({ selectedIndex }) => (
-        <>
-          <Tab.List className="grid grid-cols-3 gap-x-8">
-            {features.map((feature, featureIndex) => (
-              <Feature
-                key={feature.name}
-                feature={{
-                  ...feature,
-                  name: (
-                    <Tab className="[&:not(:focus-visible)]:focus:outline-none">
-                      <span className="absolute inset-0" />
-                      {feature.name}
-                    </Tab>
-                  ),
-                }}
-                isActive={featureIndex === selectedIndex}
-                className="relative"
-              />
-            ))}
-          </Tab.List>
-          <Tab.Panels className="relative mt-20 overflow-hidden rounded-4xl bg-slate-200 px-14 py-16 xl:px-16">
-            <div className="-mx-5 flex">
-              {features.map((feature, featureIndex) => (
-                <Tab.Panel
-                  static
-                  key={feature.name}
-                  className={clsx(
-                    'px-5 transition duration-500 ease-in-out [&:not(:focus-visible)]:focus:outline-none',
-                    featureIndex !== selectedIndex && 'opacity-60'
-                  )}
-                  style={{ transform: `translateX(-${selectedIndex * 100}%)` }}
-                  aria-hidden={featureIndex !== selectedIndex}
-                >
-                  <div className="w-[52.75rem] overflow-hidden rounded-xl bg-white shadow-lg shadow-slate-900/5 ring-1 ring-slate-500/10">
-                    <Image
-                      className="w-full"
-                      src={feature.image}
-                      alt=""
-                      sizes="52.75rem"
-                    />
-                  </div>
-                </Tab.Panel>
-              ))}
-            </div>
-            <div className="pointer-events-none absolute inset-0 rounded-4xl ring-1 ring-inset ring-slate-900/10" />
-          </Tab.Panels>
-        </>
-      )}
-    </Tab.Group>
   )
 }
 
@@ -215,15 +96,26 @@ export function Features() {
       <Container>
         <div className="mx-auto max-w-2xl md:text-center">
           <h2 className="font-display text-3xl tracking-tight text-slate-900 sm:text-4xl">
-            Simplify everyday business tasks.
+            Powerful RPG management features
           </h2>
           <p className="mt-4 text-lg tracking-tight text-slate-700">
-            Because you’d probably be a little confused if we suggested you
-            complicate your everyday business tasks instead.
+            Simplify your RPG management with features and tools that will let
+            you stop managing your game and start playing it again.
           </p>
         </div>
-        <FeaturesMobile />
-        <FeaturesDesktop />
+
+        <div className="mt-16 grid grid-cols-3 gap-x-8 gap-y-16">
+          {features.map((feature, featureIndex) => (
+              <Feature
+                key={feature.name}
+                feature={{
+                  ...feature,
+                  name: feature.name,
+                }}
+                className="relative"
+              />
+            ))}
+        </div>
       </Container>
     </section>
   )
