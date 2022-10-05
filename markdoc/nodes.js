2fe59c04b258aef96f03a30924034886ff9c7689
David VanScott <david.vanscott@gmail.com>
2022-10-05 15:59:00 -0400

    Use torchlight

--- markdoc/nodes.js
+++ markdoc/nodes.js
@@ -1,5 +1,6 @@
 import { Fence } from '@/components/Fence'
-import { nodes as defaultNodes } from '@markdoc/markdoc'
+import Markdoc, { nodes as defaultNodes } from '@markdoc/markdoc'
+import { torchlight, Block } from '@torchlight-api/torchlight-cli'
 
 const nodes = {
   document: {
@@ -18,10 +19,38 @@ const nodes = {
   fence: {
     render: Fence,
     attributes: {
-      language: {
-        type: String,
-      },
+      content: { type: String, render: false, required: true },
+      language: { type: String, render: 'data-language' },
+      process: { type: Boolean, render: false, default: true },
     },
+    async transform(node, config) {
+      const attributes = node.transformAttributes(config)
+      const children = node.children.length
+        ? node.transformChildren(config)
+        : [node.attributes.content]
+
+      torchlight.init({
+        theme: 'material-theme-palenight',
+        options: {
+          lineNumbers: true,
+          diffIndicators: true,
+          diffIndicatorsInPlaceOfLineNumbers: true,
+        }
+      })
+
+      const block = new Block({
+          language: node.attributes.language,
+          code: node.attributes.content
+      })
+
+      const { classes, highlighted, language, styles } = (await torchlight.highlight([block]))[0]
+
+      return Markdoc.createElement('Fence', {
+        ...attributes,
+        className: classes,
+        style: styles
+      }, highlighted)
+    }
   },
 }
 
