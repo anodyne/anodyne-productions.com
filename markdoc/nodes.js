0738ed2349ccf9640982417983964415f09525b8
David VanScott <david.vanscott@gmail.com>
2022-10-05 16:17:05 -0400

    Small updates

--- markdoc/nodes.js
+++ markdoc/nodes.js
@@ -3,55 +3,55 @@ import Markdoc, { nodes as defaultNodes } from '@markdoc/markdoc'
 import { torchlight, Block } from '@torchlight-api/torchlight-cli'
 
 const nodes = {
-  document: {
-    render: undefined,
-  },
-  th: {
-    ...defaultNodes.th,
-    attributes: {
-      ...defaultNodes.th.attributes,
-      scope: {
-        type: String,
-        default: 'col',
+   document: {
+      render: undefined,
+   },
+   th: {
+      ...defaultNodes.th,
+      attributes: {
+         ...defaultNodes.th.attributes,
+         scope: {
+            type: String,
+            default: 'col',
+         },
       },
-    },
-  },
-  fence: {
-    render: Fence,
-    attributes: {
-      content: { type: String, render: false, required: true },
-      language: { type: String, render: 'data-language' },
-      process: { type: Boolean, render: false, default: true },
-    },
-    async transform(node, config) {
-      const attributes = node.transformAttributes(config)
-      const children = node.children.length
-        ? node.transformChildren(config)
-        : [node.attributes.content]
+   },
+   fence: {
+      render: Fence,
+      attributes: {
+         content: { type: String, render: false, required: true },
+         language: { type: String, render: 'data-language' },
+         process: { type: Boolean, render: false, default: true },
+      },
+      async transform(node, config) {
+         const attributes = node.transformAttributes(config)
+         const children = node.children.length
+            ? node.transformChildren(config)
+            : [node.attributes.content]
 
-      torchlight.init({
-        theme: 'material-theme-palenight',
-        options: {
-          lineNumbers: true,
-          diffIndicators: true,
-          diffIndicatorsInPlaceOfLineNumbers: true,
-        }
-      })
+         torchlight.init({
+            theme: 'material-theme-palenight',
+            options: {
+               lineNumbers: false,
+               diffIndicators: true,
+               diffIndicatorsInPlaceOfLineNumbers: true,
+            }
+         })
 
-      const block = new Block({
-          language: node.attributes.language,
-          code: node.attributes.content
-      })
+         const block = new Block({
+            language: node.attributes.language,
+            code: node.attributes.content
+         })
 
-      const { classes, highlighted, language, styles } = (await torchlight.highlight([block]))[0]
+         const { classes, highlighted, language, styles } = (await torchlight.highlight([block]))[0]
 
-      return Markdoc.createElement('Fence', {
-        ...attributes,
-        className: classes,
-        style: styles
-      }, highlighted)
-    }
-  },
+         return Markdoc.createElement('Fence', {
+            ...attributes,
+            className: classes,
+            style: styles
+         }, highlighted)
+      }
+   },
 }
 
 export default nodes
