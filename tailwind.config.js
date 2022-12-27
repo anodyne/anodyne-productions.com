77c72867d6c2e7685d9b25c9383c9e0f0751fe2a
David VanScott <david.vanscott@gmail.com>
2022-12-27 16:53:02 -0500

    Updates for Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,23 +8,28 @@ module.exports = {
     darkMode: 'class',
     theme: {
         fontSize: {
-            xs: ['0.75rem', { lineHeight: '1rem' }],
+            '2xs': ['0.75rem', { lineHeight: '1.25rem' }],
+            xs: ['0.8125rem', { lineHeight: '1.5rem' }],
             sm: ['0.875rem', { lineHeight: '1.5rem' }],
             base: ['1rem', { lineHeight: '1.75rem' }],
-            lg: ['1.125rem', { lineHeight: '2rem' }],
-            xl: ['1.25rem', { lineHeight: '2rem' }],
+            lg: ['1.125rem', { lineHeight: '1.75rem' }],
+            xl: ['1.25rem', { lineHeight: '1.75rem' }],
             '2xl': ['1.5rem', { lineHeight: '2rem' }],
-            '3xl': ['2rem', { lineHeight: '2.5rem' }],
-            '4xl': ['2.5rem', { lineHeight: '3.5rem' }],
-            '5xl': ['3rem', { lineHeight: '3.5rem' }],
+            '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+            '4xl': ['2.25rem', { lineHeight: '2.5rem' }],
+            '5xl': ['3rem', { lineHeight: '1' }],
             '6xl': ['3.75rem', { lineHeight: '1' }],
-            '7xl': ['4.5rem', { lineHeight: '1.1' }],
+            '7xl': ['4.5rem', { lineHeight: '1' }],
             '8xl': ['6rem', { lineHeight: '1' }],
             '9xl': ['8rem', { lineHeight: '1' }],
         },
+        typography: require('./typography'),
         extend: {
             borderRadius: {
                 '4xl': '2rem',
+            },
+            boxShadow: {
+                glow: '0 0 4px rgb(0 0 0 / 0.1)',
             },
             fontFamily: {
                 sans: ['Inter', ...defaultTheme.fontFamily.sans],
@@ -34,8 +39,17 @@ module.exports = {
                 0: 0,
             },
             maxWidth: {
+                lg: '33rem',
                 '2xl': '40rem',
+                '3xl': '50rem',
+                '5xl': '66rem',
                 '8xl': '88rem',
+            },
+            opacity: {
+                1: '0.01',
+                2.5: '0.025',
+                7.5: '0.075',
+                15: '0.15',
             },
         },
     },
