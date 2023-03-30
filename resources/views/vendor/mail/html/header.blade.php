7c01b76178711b2f5fbe89d11a0d3d77339ebf18
David VanScott <david.vanscott@gmail.com>
2023-03-30 16:08:38 -0400

    Updates for vendor files

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="{{ asset('images/anodyne-logo-vertical-color.png') }}" height="100">
+</a>
+</td>
+</tr>
