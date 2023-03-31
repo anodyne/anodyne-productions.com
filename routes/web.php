2e27c3e80dcd6fbc92aaeeedd650a9e5bc9d274a
David VanScott <david.vanscott@gmail.com>
2023-03-31 16:55:16 -0400

    Route file cleanup

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
-use App\CommonMark\Extensions\Tag\TagExtension;
 use App\Http\Controllers\DocsController;
 use App\Http\Livewire\AddonsDisplay;
 use App\Http\Livewire\AddonsList;
-use App\Models\Addon;
 use App\Models\Release;
 use App\Models\Sponsor;
-use App\Notifications\AccountMigrated;
-use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Route;
-use League\CommonMark\Environment\Environment;
-use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
-use League\CommonMark\MarkdownConverter;
 
 Route::get('/nova', function () {
     $release = Release::query()
@@ -43,42 +36,4 @@ Route::get('/docs/{version?}/{page?}', DocsController::class)
 Route::get('/addons', AddonsList::class)->name('addons.index');
 Route::get('/addons/{addon}', AddonsDisplay::class)->name('addons.show');
 
-// Route::get('/addons/{addon}', function (Addon $addon) {
-//     return view('addons.show', compact('addon'));
-// })->name('addons.show');
-
 Route::redirect('/', '/nova');
-
-Route::get('/test', function () {
-//     $markdown = '{% tip title="A word to the wise" %}
-    // This is my **note** content.
-    // {% /tip %}';
-
-    $markdown = '{% quick-links %}
-{% quick-link title="Installation" icon="flex-cloud-download" href="/docs/2.7/installation" description="Step-by-step guide to installing Nova on your server." /%}
-{% /quick-links %}';
-
-    // $markdown = '{% quick-link title="Installation" icon="flex-cloud-download" href="/docs/2.7/installation" description="Step-by-step guide to installing Nova on your server." /%}';
-
-    // $markdown = '{% screenshot src="https://images.unsplash.com/photo-1676018526219-cac529193274?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80" alt="foo" /%}';
-
-    $environment = new Environment();
-
-    $environment->addExtension(new CommonMarkCoreExtension());
-    $environment->addExtension(new TagExtension());
-
-    $converter = new MarkdownConverter($environment);
-
-    // dd($converter->convert($markdown)->getContent());
-
-    // echo $converter->convert($markdown)->getContent();
-
-    return view('test', [
-        'markdown' => $converter->convert($markdown)->getContent(),
-    ]);
-});
-
-Route::get('email-test', function () {
-    Notification::route('mail', 'david.vanscott@gmail.com')
-        ->notify(new AccountMigrated('abc123'));
-});
