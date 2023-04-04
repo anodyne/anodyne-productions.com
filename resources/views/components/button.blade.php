e3a53bd8e0c8305081f627c8aac3e7ce7fc247df
David VanScott <david.vanscott@gmail.com>
2023-04-04 11:20:15 -0400

    Entity decoding on the button component

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,5 +1,5 @@
 @if ($href)
-  <a href="{{ $href }}" {{ $attributes->merge(['class' => "{$variantStyles()} {$sizeStyles()} {$baseStyles()}"]) }}>
+  <a href="{{ html_entity_decode($href) }}" {{ $attributes->merge(['class' => "{$variantStyles()} {$sizeStyles()} {$baseStyles()}"]) }}>
     <span class="relative z-10 flex items-center justify-center space-x-1.5">
       {{ $slot }}
     </span>
