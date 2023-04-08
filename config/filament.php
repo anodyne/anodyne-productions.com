e83f64be7e7471369424e749f9df679ee9d045cb
David VanScott <david.vanscott@gmail.com>
2023-04-07 23:16:52 -0400

    Filament config updates

--- config/filament.php
+++ config/filament.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Filament\Pages\Dashboard;
+use App\Filament\Widgets\LatestUpdates;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Http\Middleware\MirrorConfigToSubpackages;
-use Filament\Pages;
 use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
@@ -105,7 +106,7 @@ return [
         'namespace' => 'App\\Filament\\Pages',
         'path' => app_path('Filament/Pages'),
         'register' => [
-            Pages\Dashboard::class,
+            Dashboard::class,
         ],
     ],
 
@@ -141,6 +142,7 @@ return [
         'register' => [
             Widgets\AccountWidget::class,
             Widgets\FilamentInfoWidget::class,
+            LatestUpdates::class,
         ],
     ],
 
