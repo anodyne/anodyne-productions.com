eb3f8119da8f13e551b7ebc4e2133114296ed001
David VanScott <david.vanscott@gmail.com>
2023-04-10 12:06:07 -0400

    Updated styles for selects

--- resources/views/components/input/select.blade.php
+++ resources/views/components/input/select.blade.php
@@ -1,3 +1,10 @@
-<select {{ $attributes->merge(['class' => 'block w-full pl-3 pr-10 py-2 border-gray-300 focus:outline-none focus:ring-blue-400 focus:border-blue-400 rounded-lg shadow-sm']) }}>
+<select
+    @class([
+        'border-slate-200 dark:border-slate-200/[15%] focus:border-purple-400 focus:ring-purple-400 dark:focus:border-purple-600 dark:focus:ring-purple-600',
+        'form-select block w-full pl-3 pr-10 py-2 text-base bg-white dark:bg-slate-700/50 text-slate-900 dark:text-slate-100 dark:focus:bg-slate-800 focus:outline-none focus:ring-1 rounded-lg shadow-sm',
+        $attributes->get('class'),
+    ])
+    {{ $attributes }}
+>
     {{ $slot }}
 </select>
