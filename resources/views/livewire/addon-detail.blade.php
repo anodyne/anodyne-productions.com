8c351523feb1089d116579017cdf8604192d5de8
David VanScott <david.vanscott@gmail.com>
2023-06-26 22:38:07 -0400

    Fix

--- resources/views/livewire/addon-detail.blade.php
+++ resources/views/livewire/addon-detail.blade.php
@@ -294,13 +294,15 @@
                 <h3>Links</h3>
                 <dl class="space-y-3">
                   @foreach ($addon->links as $link)
-                    <div>
-                      <dt class="font-medium flex items-center space-x-2">
-                        @svg($link['type']->icon(), 'h-5 w-5 shrink-0')
-                        <span>{{ $link['type']->displayName() }}</span>
-                      </dt>
-                      <dd><a href="{{ $link['value'] }}">{{ $link['value'] }}</a></dd>
-                    </div>
+                    @if ($link['value'] !== null)
+                      <div>
+                        <dt class="font-medium flex items-center space-x-2">
+                          @svg($link['type']->icon(), 'h-5 w-5 shrink-0')
+                          <span>{{ $link['type']->displayName() }}</span>
+                        </dt>
+                        <dd><a href="{{ $link['value'] }}">{{ $link['value'] }}</a></dd>
+                      </div>
+                    @endif
                   @endforeach
                 </dl>
               @endif
