372e6a00637eb5fb71f1185fab8ca34615bd7bc4
David VanScott <david.vanscott@gmail.com>
2024-10-25 13:20:05 -0400

    Updates for scheduled tasks

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use App\Console\Commands\CheckHeartbeats;
 use App\Console\Commands\CheckInactiveHeartbeats;
 use App\Console\Commands\DeactivateInactiveGames;
+use DateTimeZone;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -19,15 +20,25 @@ class Kernel extends ConsoleKernel
 
         // $schedule->command('anodyne:cleanup-users')->daily();
 
-        $schedule->command('anodyne:fetch-sponsorship-tiers')->dailyAt('01:00')->environments(['production']);
+        $schedule->command('anodyne:fetch-sponsorship-tiers')
+            ->dailyAt('01:00')
+            ->environments(['production']);
 
-        $schedule->command('anodyne:fetch-sponsors')->dailyAt('01:30')->environments(['production']);
+        $schedule->command('anodyne:fetch-sponsors')
+            ->dailyAt('01:30')
+            ->environments(['production']);
 
-        $schedule->command(CheckHeartbeats::class)->dailyAt('02:00')->environments(['production']);
+        $schedule->command(CheckHeartbeats::class)
+            ->dailyAt('02:00')
+            ->environments(['production']);
 
-        $schedule->command(CheckInactiveHeartbeats::class)->monthlyOn(1, '02:30')->environments(['production']);
+        $schedule->command(CheckInactiveHeartbeats::class)
+            ->monthlyOn(1, '02:30')
+            ->environments(['production']);
 
-        $schedule->command(DeactivateInactiveGames::class)->dailyAt('03:00')->environments(['production']);
+        $schedule->command(DeactivateInactiveGames::class)
+            ->dailyAt('03:00')
+            ->environments(['production']);
     }
 
     /**
@@ -38,5 +49,10 @@ class Kernel extends ConsoleKernel
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
+    }
+
+    protected function scheduleTimezone(): DateTimeZone|string|null
+    {
+        return 'America/New_York';
     }
 }
