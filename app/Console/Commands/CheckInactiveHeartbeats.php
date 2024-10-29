a739f3cf33463527df6acdf8e40e7e66419ac145
David VanScott <david.vanscott@gmail.com>
2024-10-29 13:39:44 -0400

    Switch inactive games check to a batched job

--- app/Console/Commands/CheckInactiveHeartbeats.php
+++ app/Console/Commands/CheckInactiveHeartbeats.php
@@ -4,7 +4,11 @@ namespace App\Console\Commands;
 
 use App\Jobs\CheckHeartbeat;
 use App\Models\Game;
+use Illuminate\Bus\Batch;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Bus;
+use Spatie\DiscordAlerts\Facades\DiscordAlert;
+use Throwable;
 
 class CheckInactiveHeartbeats extends Command
 {
@@ -20,6 +24,29 @@ class CheckInactiveHeartbeats extends Command
             ->whereRelation('release', 'has_heartbeat_endpoint', '=', true)
             ->get();
 
-        $games->each(fn (Game $game) => dispatch(new CheckHeartbeat($game)));
+        $jobs = $games->mapInto(CheckHeartbeat::class);
+
+        Bus::batch($jobs->all())
+            ->allowFailures()
+            ->before(function (Batch $batch) {
+                // The batch has been created but no jobs have been added...
+            })->progress(function (Batch $batch) {
+                // A single job has completed successfully...
+            })->then(function (Batch $batch) {
+                // All jobs completed successfully...
+            })->catch(function (Batch $batch, Throwable $e) {
+                // First batch job failure detected...
+            })->finally(function (Batch $batch) {
+                DiscordAlert::to('alerts')
+                    ->message('Heartbeat checks on inactive games completed', [
+                        [
+                            'title' => 'Nova heartbeat checks for inactive games completed on '.now()->format('l F jS, Y'),
+                            'color' => '#fbbf24',
+                            'fields' => [
+                                ['name' => 'Inactive games', 'value' => $batch->totalJobs, 'inline' => true],
+                            ],
+                        ],
+                    ]);
+            })->name('Check heartbeats for inactive games '.now()->format('m/d/Y'))->dispatch();
     }
 }
