bb81bf4c53f2f82953677daab54a9036221398c9
David VanScott <david.vanscott@gmail.com>
2024-11-04 16:16:33 -0500

    Make sure timestamps aren't updated

--- app/Filament/Resources/GameResource/Pages/EditGame.php
+++ app/Filament/Resources/GameResource/Pages/EditGame.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\GameResource;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ViewAction;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class EditGame extends EditRecord
 {
@@ -17,5 +18,14 @@ class EditGame extends EditRecord
             ViewAction::make(),
             DeleteAction::make(),
         ];
+    }
+
+    protected function handleRecordUpdate(Model $record, array $data): Model
+    {
+        return Model::withoutTimestamps(function () use ($record, $data) {
+            $record->update($data);
+
+            return $record;
+        });
     }
 }
